b93b0ca80b35d6125d957e8a4cdfc19db6a043e8
Eissa Saber <eissapk44@gmail.com>
2023-12-06 15:05:32 +0200

    updated globals

--- Frontend/src/app/layout.js
+++ Frontend/src/app/layout.js
@@ -1,45 +1,65 @@
 import "@/styles/globals.scss";
 import localFont from "next/font/local";
 
-const santoshiLightFont = localFont({
+// primary
+const primaryLightFont = localFont({
   src: [{ path: "./assets/fonts/Satoshi-Light.ttf", style: "normal", display: "swap" }],
-  variable: "--lightFont",
+  variable: "--primaryLightFont",
 });
-
-const santoshiRegularFont = localFont({
+const primaryRegularFont = localFont({
   src: [{ path: "./assets/fonts/Satoshi-Regular.ttf", style: "normal", display: "swap" }],
-  variable: "--regularFont",
+  variable: "--primaryRegularFont",
 });
-
-const santoshiMediumFont = localFont({
+const primaryMediumFont = localFont({
   src: [{ path: "./assets/fonts/Satoshi-Medium.ttf", style: "normal", display: "swap" }],
-  variable: "--mediumFont",
+  variable: "--primaryMediumFont",
 });
 
-const ltZaridDisplayFont = localFont({
+// secondary
+const secondaryLightFont = localFont({
+  src: [{ path: "./assets/fonts/LTZaridDisplay-Light.ttf", style: "normal", display: "swap" }],
+  variable: "--secondaryLightFont",
+});
+const secondaryRegularFont = localFont({
+  src: [{ path: "./assets/fonts/LTZaridSans-Regular.ttf", style: "normal", display: "swap" }],
+  variable: "--secondaryRegularFont",
+});
+const secondaryMediumFont = localFont({
+  src: [{ path: "./assets/fonts/LTZaridDisplay-Medium.ttf", style: "normal", display: "swap" }],
+  variable: "--secondaryMediumFont",
+});
+const secondaryBoldFont = localFont({
   src: [{ path: "./assets/fonts/LTZaridSans-SemiBold.ttf", style: "normal", display: "swap" }],
-  variable: "--boldFont",
+  variable: "--secondaryBoldFont",
 });
 
 export const metadata = {
-  title: "Saudi artisanal company",
+  title: "SAC | Saudi artisanal company",
   description: "Inspired by the generosity and bestowments of the palm tree.",
 };
 
 export default function RootLayout({ children, params }) {
+  console.log({ params });
   const locale = params.lang || "en";
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
       <body
-        className={`${santoshiLightFont.className}
-        ${santoshiLightFont.variable}
-        ${santoshiRegularFont.className}
-        ${santoshiRegularFont.variable}
-        ${santoshiMediumFont.className}
-        ${santoshiMediumFont.variable}
+        className={`
+        ${primaryLightFont.className}
+        ${primaryLightFont.variable}
+        ${primaryRegularFont.className}
+        ${primaryRegularFont.variable}
+        ${primaryMediumFont.className}
+        ${primaryMediumFont.variable}
 
-        ${ltZaridDisplayFont.className}
-        ${ltZaridDisplayFont.variable}`}
+        ${secondaryLightFont.className}
+        ${secondaryLightFont.variable}
+        ${secondaryRegularFont.className}
+        ${secondaryRegularFont.variable}
+        ${secondaryMediumFont.className}
+        ${secondaryMediumFont.variable}
+        ${secondaryBoldFont.className}
+        ${secondaryBoldFont.variable}`}
       >
         {children}
       </body>
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+      screens: {
+        sm: "600px",
+        md: "962px",
+        lg: "1100px",
+        xl: "1228px",
+      },
+    extend: {
+      margin: {
+        1: "1vw",
+        2: "1.3vw",
+        3: "3vw",
+      },
+      padding: {
+        1: "1vw",
+        2: "1.3vw",
+        3: "3vw",
+      },
+    },
   },
   plugins: [],
 }
